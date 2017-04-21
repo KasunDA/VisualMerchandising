f53c2a9e421cf51540287fe719f9dfaa5fea694c
Stabja Hazra <Stabja Hazra>
2017-04-21 20:51:13 +0530

    Just Now

--- app/src/main/java/com/tophawks/vm/visualmerchandising/fragment/SalesAccountsFragment.java
+++ app/src/main/java/com/tophawks/vm/visualmerchandising/fragment/SalesAccountsFragment.java
@@ -22,7 +22,6 @@ public class SalesAccountsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_sales_accounts, container, false);
     }
 
